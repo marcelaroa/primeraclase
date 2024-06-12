bee60ebe125c33e7f6038b1dfde8f42a848093df
lolita0502 <roa.roaet12de1@gmail.com>
2024-06-11 21:59:58 -0300

    first commit

---
+++ clases.cs
@@ -0,0 +1,20 @@
+namespace LibreriaClases{
+
+    class perro{
+
+    public string raza;
+    public int edad;
+    public string tamano;
+
+
+    public void jugar(string pjugar){
+
+        Console.WriteLine("jugando..."+ pjugar);
+
+    }
+    public string ladrar(){
+
+        return "guau guau...";
+    }
+    }
+}
---
+++ principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LibreriaClases;
+
+namespace principal {
+
+    class programaPrincipal{
+        static void Main(){
+
+            string nombre;
+            nombre = "juan";
+
+            perro firulai;
+
+            firulai = new perro();
+
+            perro pedro = new perro();
+
+            Console.WriteLine("hola");
+
+           firulai.edad = 2;
+
+           firulai.tamano = "grande";
+
+           firulai.raza = "caniche";
+
+           firulai.jugar("saltar");
+           firulai.jugar("dormirse");
+
+           Console.WriteLine(firulai.ladrar());
+        }
+    }
+}
